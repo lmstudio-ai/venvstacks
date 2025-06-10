9174984789a809898a1e28caebba176b02580ee9
Alyssa Coghlan <ncoghlan@gmail.com>
2025-06-11 02:15:33 +1000

    Update jupyterlab example stack lock
    Also add a helper script to refresh the example stack locks.
    
    Resolves dependendabot warnings about a requests CVE.

---
+++ misc/lock-example-stacks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# See http://redsymbol.net/articles/unofficial-bash-strict-mode/ for benefit of these options
+set -euo pipefail
+IFS=$'\n\t'
+
+# Note: `readlink -f` (long available in GNU coreutils) is available on macOS 12.3 and later
+script_dir="$(cd -- "$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")" &> /dev/null && pwd)"
+
+lock_stack () {
+    local stack_spec=${1:?}
+    venvstacks lock --reset-lock='*' "$stack_spec"
+}
+
+for entry in "$script_dir/../examples/"*; do
+    if [ -d "$entry" ]; then
+        lock_stack "$entry/venvstacks.toml"
+    fi
+done
